09f15339dea216b977f6fb96d4f5e375479a6e9b
kumaramit31 <137507051+kumaramit31@users.noreply.github.com>
2023-07-29 21:15:34 +0530

    Add files via upload

---
+++ circuits/multiplier/circuit.circom
@@ -0,0 +1,56 @@
+pragma circom 2.0.0;
+
+/*This circuit template checks that c is the multiplication of a and b.*/  
+
+template Multiplier2 () {  
+
+   // Declaration of signals.  
+   signal input a;  
+   signal input b;
+   signal x;  
+   signal y;  
+   signal output c;
+
+   //component
+   component andGate = AND();
+   component notGate = NOT();
+   component orGate = OR();
+
+   // Circuit Logic.  
+   andGate.a <== a;
+   andGate.b <== b;
+   x <== andGate.out;
+
+   notGate.in <== b;
+   y <== notGate.out;
+
+   orGate.a <== x;
+   orGate.b <== y;
+   c <== orGate.out;
+
+}
+
+template AND() {
+    signal input a;
+    signal input b;
+    signal output out;
+
+    out <== a*b;
+}
+
+template NOT() {
+    signal input in;
+    signal output out;
+
+    out <== 1 + in - 2*in;
+}
+
+template OR() {
+    signal input a;
+    signal input b;
+    signal output out;
+
+    out <== a + b - a*b;
+}
+
+component main = Multiplier2();
---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// https://github.com/projectsophon/hardhat-circom
+import "hardhat-circom";
+// circuits
+import circuits = require('./circuits.config.json')
+
+// set env var to the root of the project
+process.env.BASE_PATH = __dirname;
+
+// tasks
+import "./tasks/newcircuit.ts"
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  networks: {
+    mumbai:{
+      url:`https://polygon-mumbai.g.alchemy.com/v2/demo`,
+      accounts:["493b800962da871ed8358ac02e802acada54ce11b3d4c9a678b6f8d863fd8ed6"]
+  }
+},
+  circom: {
+    // (optional) Base path for input files, defaults to `./circuits/`
+    inputBasePath: "./circuits",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+};
+
+export default config;
